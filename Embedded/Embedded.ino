adbdf26d8dc951ef575cd7e71e6dc68e7aa7f483
Greg Toombs <greg.toombs@no.spam>
2013-04-13 14:32:14 -0400

    Intellisense support for Arduino

--- Embedded/Embedded.ino
+++ Embedded/Embedded.ino
@@ -1,5 +1,21 @@
+// For Intellisense
+#ifdef _MSC_VER
+#	define __AVR_ATmega328P__
+#	include <Arduino.h>
+#endif
+
 void setup()
 {
+	pinMode(A0, INPUT);
+	pinMode(A1, INPUT);
+	pinMode(A2, INPUT);
+	pinMode(A3, INPUT);
+	pinMode(A4, INPUT);
+	pinMode(A5, INPUT);
+
+	analogReference(INTERNAL);
+
+	Serial.begin(2000000);
 }
 
 void loop()
