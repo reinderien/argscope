f2db6c05582e1f94a4a46e5604ec72317fe20ddc
Greg Toombs <greg.toombs@no.spam>
2013-04-16 01:06:55 -0400

    Clean up refs

--- Gui/Arduino/Hotplug.cs
+++ Gui/Arduino/Hotplug.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Management;
-using System.Windows;
 
 namespace Argscope.Arduino
 {
--- Gui/ScopeControl.xaml.cs
+++ Gui/ScopeControl.xaml.cs
@@ -1,11 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Collections.Specialized;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Media;
 
 namespace Argscope
 {
